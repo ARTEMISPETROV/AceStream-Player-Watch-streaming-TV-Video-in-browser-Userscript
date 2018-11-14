f03ec2270dfd7520dc2790e7d866c84519030f4c
Artem Petrov <petrovartem@protonmail.com>
2018-11-14 16:40:17 +0300

    Add files via upload

---
+++ AceStreamLinks.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name AceStream links open TV Video streaming links in browser
+// @namespace https://github.com/ARTEMISPETROV
+// @description Replaces acestream://  with 127.0.0.1:6878/webui/player/
+// @include http://*
+// @include https://*
+// @version 1.0
+// @author Artem Petrov <petrovartem@protonmail.com> ( https://github.com/ARTEMISPETROV)
+// @source https://github.com/ARTEMISPETROV/AceStream-Player-Watch-streaming-TV-Video-in-browser-Userscript
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_deleteValue
+// @grant GM.setValue
+// @grant GM.getValue
+// @grant GM.deleteValue
+// @run-at document-start
+// @license MIT
+// ==/UserScript==
+
+var url1,url2;
+url1 = ['acestream://'];
+url2 = ['127.0.0.1:6878/webui/player/'];
+url3 = ['?autoplay=true'];
+var a, links;
+var tmp="a";
+var p,q;
+links = document.getElementsByTagName('a');
+for (var i = 0; i < links.length; i++) {
+    a = links[i];
+    for(var j=0;j<url1.length; j++)
+	{
+	tmp = a.href+"" ;
+	if(tmp.indexOf(url1[j]) != -1)
+	{
+	p=tmp.indexOf(url1[j]) ;
+	q="http://";
+	q = q + url2[j] + tmp.substring(p+url1[j].length,tmp.length)+ url3;
+	a.href=q ;
+	}
+	}
+    }
